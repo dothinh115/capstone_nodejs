35aff7b56258800280f94267e6240050ad78303c
dothinh115 <dothinh115@gmail.com>
2023-03-25 12:09:32 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api');
   app.use(express.static('.'));
+
   await app.listen(process.env.PORT);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
 //yarn add @nestjs/swagger swagger-ui-express
